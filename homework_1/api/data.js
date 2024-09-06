bacbffb75051b7cea18091fb7811184b1546c8da
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-06 12:04:26 +0300

    added fake api

---
+++ homework_1/api/data.js
@@ -0,0 +1,41 @@
+export const data = [
+	{
+		"uid": "1",
+		"messages": [
+			{
+				"id": "f17H",
+				"date": "2023-01-01",
+				"text": "hi"
+			},
+			{
+				"id": "qrty",
+				"date": "2023-01-02",
+				"text": "how are you?"
+			}
+		],
+		"posts": []
+	},
+	{
+		"uid": "2",
+		"messages": [
+			{
+				"id": "gd5u",
+				"date": "2023-01-01",
+				"text": "hi"
+			},
+			{
+				"id": "7uv9",
+				"date": "2023-01-03",
+				"text": "fine, thanks!"
+			}
+		],
+		"posts": [
+			{
+				"id": "ggwp-1337",
+				"date": "2023-01-03",
+				"label": "wake up",
+				"text": "you are still sleeping"
+			}
+		]
+	}
+]
---
+++ homework_1/api/users.js
@@ -0,0 +1,16 @@
+export const users = [
+	{
+		"id": "1",
+		"name": "John",
+		"sirname": "Doe",
+		"login": "Johny_doe@fake.com",
+		"password": "Lullaby_for"
+	},
+	{
+		"id": "2",
+		"name": "Jane",
+		"sirname": "Doe",
+		"login": "Jann@fake.com",
+		"password": "a_fat_jellyfish"
+	}
+]
