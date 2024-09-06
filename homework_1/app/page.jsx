e58c8efb3914c815562f05df431615d60ac88994
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-06 16:03:24 +0300

    added Error, loading, fetchedUserData states view and logic

--- homework_1/app/page.jsx
+++ homework_1/app/page.jsx
@@ -1,9 +1,66 @@
+"use client"
+
+import { Header } from "@/components/header";
 import { LoginForm } from "@/components/loginForm";
 
+import { fetchApi, mockFetchApi } from '@/service/fetchApi'
+import { logger } from '@/utils/logger'
+
+import { useState } from "react";
+
 export default function Home() {
+
+	const [error, setError] = useState()
+	const [isLoading, setIsLoading] = useState(false)
+	const [fetchedUserData, setFetchedUserData] = useState(null)
+
+	// Обычно аутентификация происходит отдельным запросом от запроса на получения данных. При этом используется хук useEffect. Но в нашем примере нет сервера. JWT токенов и куки тоже нет.
+	async function onLoginAttempt(e) {
+		e.preventDefault()
+		setIsLoading(true)
+
+		const formData = new FormData(e.currentTarget), data = {}
+
+		// В не мокнутой версии fetch есть стандартные параметры. Если отправлять formdata, то     headers[content-type] нельзя указывать. Поэтому перезапишем headers.
+		mockFetchApi('/api/data', { body: formData, headers: undefined })
+			.then(data => (setFetchedUserData(data), setError(null), data))
+			.then(logger())
+			.catch(e => (setError(e), logger('error')(e)))
+			.finally(_ => setIsLoading(false))
+	}
+
+	function logout() {
+		setFetchedUserData(null)
+	}
+
 	return (
-		<main className="main">
-			<LoginForm />
-		</main>
+		<>
+			<Header className="p-20">
+				{error && (error?.name !== 'noSuchUser') &&
+					<div className="text_lil-title red">
+						{'Упс:( Что-то пошло не так'}
+					</div>
+				}
+				{isLoading && <div>Loading...</div>}
+				{fetchedUserData &&
+					<>
+						<div className="text_lil-title mb-20">
+							Вы вошли! Смотрите данные в консоли
+						</div>
+						<button className="btn" onClick={logout}>
+							Выйти
+						</button>
+					</>
+				}
+			</Header>
+			<main className="main">
+				{!fetchedUserData &&
+					<LoginForm
+						onSubmit={onLoginAttempt}
+						error={error}
+					/>
+				}
+			</main>
+		</>
 	);
 }
--- homework_1/components/loginForm/loginForm.jsx
+++ homework_1/components/loginForm/loginForm.jsx
@@ -1,30 +1,7 @@
-"use client"
 
-import { useState } from 'react'
 import './loginForm.css'
 
-import { fetchApi, mockFetchApi } from '@/service/network/fetchApi'
-import { logger } from '@/utils/logger'
-
-export function LoginForm() {
-
-	const [data, setData] = useState()
-	const [error, setError] = useState()
-
-	async function onSubmit(e) {
-		e.preventDefault()
-
-		const formData = new FormData(e.currentTarget), data = {}
-
-		// В не мокнутой версии fetch есть стандартные параметры. Если отправлять formdata, то content-type нельзя указывать. Поэтому перезапишем headers.
-		mockFetchApi('/api/data', { body: formData, headers: undefined })
-			.then(data => (setData(data), data))
-			.then(logger())
-			.catch(e => (setError(e), logger('error')(e)))
-	}
-
-
-
+export function LoginForm({ onSubmit, error }) {
 
 	return (
 		<div className="input">
@@ -39,8 +16,10 @@ export function LoginForm() {
 				<label className="input__label" htmlFor="password">Пароль</label>
 				<input className="input__field" id="password" name="password" type="password" required />
 
+				{error?.name === 'noSuchUser' &&
+					<div className="calc-error red">{error.message}</div>
+				}
 				<button className="input__btn btn" type="submit" >Войти</button>
-				<div className="calc-error green"></div>
 			</form>
 		</div>
 	)
