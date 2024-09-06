f8686cf14bc68f81295daad179427d923798ba56
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-06 19:51:05 +0300

    loginForm restyling

--- homework_1/components/loginForm/loginForm.jsx
+++ homework_1/components/loginForm/loginForm.jsx
@@ -4,22 +4,22 @@ import './loginForm.css'
 export function LoginForm({ onSubmit, error }) {
 
 	return (
-		<div className="input">
-			<form className="input__form violet-border text_fields"
+		<div className="login-form">
+			<form className="login-form__form border_violet text_fields"
 				action="#" method="POST" onSubmit={onSubmit}
 			>
-				<div className="input__form-title text_lil-title">Вход в аккаунт</div>
+				<div className="login-form__title text_lil-title">Вход в аккаунт</div>
 
-				<label className="input__label" htmlFor="login">Почта</label>
-				<input className="input__field" id="login" name="login" type="email" required />
+				<div className="login-form__label"><label htmlFor="login">Почта</label></div>
+				<input className="login-form__field" id="login" name="login" type="email" required />
 
-				<label className="input__label" htmlFor="password">Пароль</label>
-				<input className="input__field" id="password" name="password" type="password" required />
+				<div className='login-form__label'><label htmlFor="password">Пароль</label></div>
+				<input className="login-form__field" id="password" name="password" type="password" required />
 
 				{error?.name === 'noSuchUser' &&
-					<div className="calc-error red">{error.message}</div>
+					<div className="input-error red">{error.message}</div>
 				}
-				<button className="input__btn btn" type="submit" >Войти</button>
+				<button className="login-form__btn btn" type="submit" >Войти</button>
 			</form>
 		</div>
 	)
